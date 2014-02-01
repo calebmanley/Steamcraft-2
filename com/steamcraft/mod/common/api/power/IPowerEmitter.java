885c54e1d54bc9a943b9cd990ddf4e8eee1a0b95
Johnny Eatmon <jmaeatmon@gmail.com>
2014-02-01 12:14:14 -0500

    More power system stuff = more errors!

--- com/steamcraft/mod/common/api/power/IPowerEmitter.java
+++ com/steamcraft/mod/common/api/power/IPowerEmitter.java
@@ -9,15 +9,20 @@
  * Steamcraft (c) Proloe 2011
  * (http://www.minecraftforum.net/topic/251532-181-steamcraft-source-code-releasedmlv054wip/)
  * 
- * File created @ [Feb 1, 2014, 11:42:49 AM]
+ * File created @ [Feb 1, 2014, 11:55:01 AM]
  */
 package com.steamcraft.mod.common.api.power;
 
+import net.minecraftforge.common.ForgeDirection;
+
 /**
+ * This Tile Entity interface allows you to indicate that a block can emit power
+ * from a specific side.
+ * 
  * @author MrArcane111
  *
  */
-public class IPowerEmitter 
+public interface IPowerEmitter 
 {
-
+	public boolean canEmitPowerFromSide(ForgeDirection side);
 }
